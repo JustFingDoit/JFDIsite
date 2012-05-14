f7248c1168e5ef560953018ea7d540365bf14227
Yo Sub Kwon <yo@subkwon.com>
2012-05-13 20:23:57 -0700

    Base rails install. Initial commit.

---
+++ justfingdoit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justfingdoit::Application.config.secret_token = '9909d9722779fa7606418ecd5cbcef1c7e5364274b1adec95bb353e9f23da0efcabc483a65d7fd5a9e952c98100a866693f2aba40b6811ca4061b7c02b103795'
